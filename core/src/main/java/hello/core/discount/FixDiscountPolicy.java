e5244d6a2247d4883f2450ac0bedadad778b2f9c
Seonguk <xsungwook3x@naver.com>
2022-04-27 14:07:53 +0900

    jpa study

---
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private  int discountFixAmmount=1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return discountFixAmmount;
+        }else{
+            return 0;
+        }
+    }
+}
