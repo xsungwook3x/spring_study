7108a195699ef0856c6746f491a2a5a1e8f2a575
Seonguk <xsungwook3x@naver.com>
2022-12-04 15:41:55 +0900

    finish setting

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        //then
+        Member findmember=memberRepository.find(saveId);
+        Assertions.assertThat(findmember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findmember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findmember).isEqualTo(member);
+    }
+}
